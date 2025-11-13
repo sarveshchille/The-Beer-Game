9b6b264a05b6dd01c3006ccabda761763274d59e
Tushar9184 <sabharwaltushar02@gmail.com>
2025-11-14 03:01:47 +0530

    Update PingController.java

--- backend/backend/src/main/java/com/beergame/backend/controller/PingController.java
+++ backend/backend/src/main/java/com/beergame/backend/controller/PingController.java
@@ -1,12 +1,14 @@
 package com.beergame.backend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
     @GetMapping("/ping")
-    public String ping() {
-        return "alive";
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("OK");
     }
+
 }
