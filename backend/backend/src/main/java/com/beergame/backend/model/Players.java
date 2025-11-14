6eeb1c96b7b27bd7e15ea03fe9ec2f41c6289b8f
Tushar9184 <sabharwaltushar02@gmail.com>
2025-11-14 22:43:37 +0530

    Update Players.java

--- backend/backend/src/main/java/com/beergame/backend/model/Players.java
+++ backend/backend/src/main/java/com/beergame/backend/model/Players.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -36,7 +35,7 @@ public class Players {
     @Column(name = "userName", nullable = false)
     private String userName;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "playerInfoId", referencedColumnName = "id")
     private PlayerInfo playerInfo;
 
