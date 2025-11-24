ce651cb30a40561bba36590fffcba3b149b2aca0
Vinamra-tech <vinamrat4@gmail.com>
2025-11-25 00:15:09 +0530

    Update GameService.java

--- backend/backend/src/main/java/com/beergame/backend/service/GameService.java
+++ backend/backend/src/main/java/com/beergame/backend/service/GameService.java
@@ -28,7 +28,6 @@ import org.springframework.transaction.support.TransactionSynchronizationAdapter
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -36,7 +35,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
