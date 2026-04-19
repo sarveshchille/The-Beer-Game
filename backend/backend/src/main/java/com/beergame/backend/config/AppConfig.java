39835ea7b18018377a1fac9896b43218a31a2530
Tushar9184 <sabharwaltushar02@gmail.com>
2026-04-19 05:59:36 +0530

    yes

--- backend/backend/src/main/java/com/beergame/backend/config/AppConfig.java
+++ backend/backend/src/main/java/com/beergame/backend/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.beergame.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,8 +10,13 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
 
-        return new RestTemplate();
+        // Allow up to 35 seconds for Render cold start
+        factory.setConnectTimeout(35_000);
+        factory.setReadTimeout(35_000);
+
+        return new RestTemplate(factory);
     }
 
 }
